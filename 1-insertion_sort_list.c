2a824c57eb75d4715488535d9e181e4f3e64507b
krisCrossApplesauce <karisrr2203@gmail.com>
2023-04-21 10:17:10 -0700

    1-insertion_sort_list.c: redid the loops so neater, also, the issue was the order in which i set the nexts and prevs of the two swapping nodes so i fixed that

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,51 +10,26 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node = (listint_t *) malloc(sizeof(listint_t));
-	listint_t *nod1 = (listint_t *) malloc(sizeof(listint_t));
-	listint_t *nod2 = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *node, *nod1, *next;
 
-	for (node = (*list); node != NULL; node = node->next)
+	for (node = (*list)->next; node != NULL; node = next)
 	{
-		nod1 = node->next;
+		next = node->next;
 
-		if (nod1 != NULL)
+		for (nod1 = node->prev; nod1 != NULL && nod1->n > node->n; nod1 = node->prev)
 		{
-			if (node->n > nod1->n)
-			{
-				node->next = nod1->next;
-				nod1->prev = node->prev;
-				if (node->next != NULL)
-				{ node->next->prev = node; }
-				if (nod1->prev != NULL)
-				{ nod1->prev->next = nod1; }
-				nod1->next = node;
-				node->prev = nod1;
+			nod1->next = node->next;
+			if (node->next != NULL)
+			{ node->next->prev = nod1; }
+			node->next = nod1;
+			node->prev = nod1->prev;
+			nod1->prev = node;
+			if (node->prev != NULL)
+			{ node->prev->next = node; }
+			else
+			{ (*list) = node; }
 
-				print_list(*list);
-
-				for (; nod1 != NULL; nod1 = nod1->prev)
-				{
-					nod2 = nod1->prev;
-
-					if (nod2 != NULL)
-					{
-						if (nod1->n < nod2->n)
-						{
-							nod2->next = nod1->next;
-							nod1->prev = nod2->prev;
-							if (nod2->next != NULL)
-							{ nod2->next->prev = nod2; }
-							if (nod1->prev != NULL)
-							{ nod1->prev->next = nod1; }
-							nod1->next = nod2;
-							nod2->prev = nod1;
-
-							print_list(*list);
-						}
-					}
-				}
-			}
+			print_list(*list);
 		}
 	}
 }
