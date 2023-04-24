babc3ced323c0cb798fe7181d7b41f17ed95f1a7
krisCrossApplesauce <karisrr2203@gmail.com>
2023-04-24 14:42:24 -0700

    made the files for 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+}
